c9a368e3d817d2bd399de671c65744bbdef3f543
eggme <kyyeto9984@naver.com>
2019-08-08 20:30:26 +0900

    드론추가
    드론추가

---
+++ Scripts/DroneContoller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneContoller : MonoBehaviour{
+    // Start is called before the first frame update
+    [SerializeField] Transform tf_Cam;
+    [SerializeField] float runSpeed;
+    [SerializeField] float walkSpped;
+    [SerializeField] float fieldLookLimitX;
+    [SerializeField] float fieldSensitivity;
+    CharacterController characterController;
+    Animator animator;
+    private MovingController mc;
+    private Rigidbody rigidbody;
+
+    private Animator anim;
+    private CapsuleCollider col;
+    void Start(){
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+        DroneData droneData = new DroneData(runSpeed, walkSpped, fieldLookLimitX, fieldSensitivity);
+        mc = new MovingController(GetComponent<Transform>(), tf_Cam, droneData);
+        rigidbody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator> ();
+        col = GetComponent<CapsuleCollider> ();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
---
+++ Scripts/DroneData.cs
@@ -0,0 +1,38 @@
+﻿public class DroneData{
+    private float runSpeed;
+    private float walkSpped;
+    private float fieldLookLimitX;
+    private float fieldSensitivity;
+
+    public DroneData(float runSpeed, float walkSpped, float fieldLookLimitX, float fieldSensitivity){
+        this.runSpeed = runSpeed;
+        this.walkSpped = walkSpped;
+        this.fieldLookLimitX = fieldLookLimitX;
+        this.fieldSensitivity = fieldSensitivity;
+    }
+
+    public float getRunSpeed(){
+        return runSpeed;
+    }
+    public void set(float runSpeed){
+        this.runSpeed = runSpeed;
+    }
+    public float getWalkSpped(){
+        return walkSpped;
+    }
+    public void setWalkSpped(float walkSpped){
+        this.walkSpped = walkSpped;
+    }
+    public float getFieldLookLimitX(){
+        return fieldLookLimitX;
+    }
+    public void setFieldLookLimitX(float fieldLookLimitX){
+        this.fieldLookLimitX = fieldLookLimitX;
+    }
+    public float getFieldSensitivity(){
+        return fieldSensitivity;
+    }
+    public void setFieldSensitivity(float fieldSensitivity){
+        this.fieldSensitivity = fieldSensitivity;
+    }
+}
